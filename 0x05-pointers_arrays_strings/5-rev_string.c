f3e1639ac4ea1467aa14adf3fb9ff5916a4f1a01
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2023-02-27 14:21:37 -0800

    5 again

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,14 @@
  *@s: this is the pointer that point to a string
  */
 void rev_string(char *s){
-	    // declare variable
+	    
     int i, len, temp;
-    len = strlen(str1); // use strlen() to get the length of str string
+    len = strlen(str1);
 
-    // use for loop to iterate the string
+
     for (i = 0; i < len/2; i++)
     {
-        // temp variable use to temporary hold the string
+    
         temp = str1[i];
         str1[i] = str1[len - i - 1];
         str1[len - i - 1] = temp;
