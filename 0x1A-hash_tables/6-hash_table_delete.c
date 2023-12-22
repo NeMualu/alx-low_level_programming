bc2f74d31f4ecdbd9019a58af2bbd890423aa5ca
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2023-12-22 20:41:28 +0000

    My first commit

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,31 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * destroy_hash_table - Deletes a hash table.
- * @ht: A pointer to a hash table.
+ * remove_hash_table - Frees and removes a hash table.
+ * @ht: A pointer to the hash table to be removed.
  */
-void destroy_hash_table(hash_table_t *ht)
+void remove_hash_table(hash_table_t *ht)
 {
-    hash_table_t *head = ht;
     hash_node_t *current_node, *next_node;
-    unsigned long int i;
+    unsigned long int index;
 
-    for (i = 0; i < ht->size; i++)
+    if (!ht) return;
+
+    for (index = 0; index < ht->size; ++index)
     {
-        if (ht->array[i] != NULL)
+        current_node = ht->array[index];
+        while (current_node != NULL)
         {
-            current_node = ht->array[i];
-            while (current_node != NULL)
-            {
-                next_node = current_node->next;
-                free(current_node->key);
-                free(current_node->value);
-                free(current_node);
-                current_node = next_node;
-            }
+            next_node = current_node->next;
+            free(current_node->key);
+            free(current_node->value);
+            free(current_node);
+            current_node = next_node;
         }
     }
-    free(head->array);
-    free(head);
+    free(ht->array);
+    free(ht);
 }
 
