111fab28b01eb663c29153d7b0aa4957806318ed
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2023-12-23 19:04:50 +0000

    My first commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,31 +1,32 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Outputs each element in a doubly linked list
+ * print_dlistint - Displays the elements of a dlistint_t linked list
  *
- * @head_node: Pointer to the first node of the list
- * Return: Number of nodes in the list
+ * @h: Pointer to the start of the list
+ * Return: Total count of the nodes in the list
  */
-size_t print_dlistint(const dlistint_t *head_node)
+size_t print_dlistint(const dlistint_t *h)
 {
-    size_t nodes_total = 0;
+    int node_count = 0;
 
-    // Confirm the list is not empty
-    if (head_node == NULL)
-        return nodes_total;
-
-    // Navigate to the beginning of the list
-    while (head_node->prev != NULL)
-        head_node = head_node->prev;
-
-    // Loop through the list and print each element
-    while (head_node)
-    {
-        printf("%d\n", head_node->n);
-        nodes_total++;
-        head_node = head_node->next;
+    // Check if the list is not empty
+    if (h == NULL) {
+        return node_count;
     }
 
-    return nodes_total;
+    // Navigate to the beginning of the list
+    while (h->prev != NULL) {
+        h = h->prev;
+    }
+
+    // Loop through the list and print each element
+    while (h != NULL) {
+        printf("%d\n", h->n);
+        node_count++;
+        h = h->next;
+    }
+
+    return node_count;
 }
 
